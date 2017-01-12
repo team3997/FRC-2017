e2269fb9b85273ab621a6b79125e8d2d2a4a383e
Michael Chacko <mikechacko10@gmail.com>
2017-01-12 11:35:03 -0600

    master setup

---
+++ src/MainProgram.cpp
@@ -0,0 +1,39 @@
+#include "WPILib.h"
+#include <string.h>
+
+class MainProgram: public frc::IterativeRobot {
+public:
+  MainProgram(void) {
+	  
+  }
+private:
+  void RobotInit() {
+
+  }
+
+  void AutonomousInit() {
+
+  }
+
+  void AutonomousPeriodic() {
+
+  }
+
+  void TeleopInit() {
+
+  }
+
+  void TeleopPeriodic() {
+
+  }
+
+  void DisabledInit() {
+
+  }
+
+  void DisabledPeriodic() {
+
+  }
+};
+
+START_ROBOT_CLASS(MainProgram);
