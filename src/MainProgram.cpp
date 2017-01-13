e5f68dcb55f560f42d97d8ca9cac1aa718f1851b
Michael Chacko <mikechacko10@gmail.com>
2017-01-12 18:11:11 -0600

    update michael branch to remove plan files

--- src/MainProgram.cpp
+++ src/MainProgram.cpp
@@ -1,23 +1,12 @@
 #include "WPILib.h"
-#include "RobotModel.h"
-#include "RemoteControl.h"
-#include "ControlBoard.h"
 #include <string.h>
 
 class MainProgram: public frc::IterativeRobot {
-  //LiveWindow helps in Test mode
-  LiveWindow *lw;
-  //Creates a robot from class RobotModel
-  RobotModel *robot;
-  //Creates a human control from RemoteControl, which includes ControlBoard
-  RemoteControl *humanControl;
 public:
   MainProgram(void) {
-    robot = new RobotModel();
-    humanControl = new ControlBoard();
+
   }
 private:
-
   void RobotInit() {
 
   }
@@ -45,7 +34,6 @@ private:
   void DisabledPeriodic() {
 
   }
-
 };
 
 START_ROBOT_CLASS(MainProgram);
