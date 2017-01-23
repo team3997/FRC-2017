3cd12e34684fc1a43ca4008485e4ca5c0aef5cd3
Michael Chacko <mikechacko10@gmail.com>
2017-01-22 18:40:28 -0600

    added input for the xbox controller

--- src/Ports.h
+++ src/Ports.h
@@ -25,20 +25,11 @@ static const int RIGHT_DRIVE_MOTOR_B_PWM_PORT           = 3;
 // ***************** JOYSTICK USB PORTS *****************
 //MAKE SURE JOYSTICKS ARE SET TO "D" position on back
 
-static const bool USING_WIN_DRIVER_STATION              = true;
+static const bool USING_WIN_DRIVER_STATION              = false;
 static const int DRIVER_JOY_USB_PORT                    = 0;
 static const int OPERATOR_JOY_USB_PORT                  = 1;
 // ***************** BUTTONS *****************
 
-static const int F310_WIN_LEFT_X_AXIS                   = 0;
-static const int F310_WIN_LEFT_Y_AXIS                   = 1;
-static const int F310_WIN_RIGHT_X_AXIS                  = 2;
-static const int F310_WIN_RIGHT_Y_AXIS                  = 3;
-
-static const int F310_LINUX_LEFT_X_AXIS                 = 0;
-static const int F310_LINUX_LEFT_Y_AXIS                 = 1;
-static const int F310_LINUX_RIGHT_X_AXIS                = 2;
-static const int F310_LINUX_RIGHT_Y_AXIS                = 3;
 
 //Drive controller button ports and preferences
 
