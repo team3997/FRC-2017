79195a12a132938e2446c0101c5fe04078db529a
Peter Chacko <workpraytoday@gmail.com>
2017-03-10 19:59:13 -0600

    added class files

---
+++ src/Auto/Routines/LeftGear.cpp
@@ -0,0 +1,21 @@
+/*
+ * LeftGear.cpp
+ *
+ *  Created on: Mar 10, 2017
+ *      Author: peter
+ */
+
+#include "LeftGear.h"
+
+LeftGear::LeftGear(RobotModel *robot, DriveController* driveTrain) {
+    this->robot = robot;
+    this->driveTrain = driveTrain;
+}
+
+void LeftGear::Prestart() {
+
+}
+
+void LeftGear::Routine() {
+
+}
